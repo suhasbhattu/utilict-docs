ff8512332465c48a08d33b119f0564becb425d11
Suhas Bhattu <suhasbhattu@gmail.com>
2024-12-08 12:31:38 +0530

    initial commit

---
+++ pages/dataTypes/_meta.js
@@ -0,0 +1,5 @@
+export default {
+  number: "Number",
+  string: "String",
+  array: "Array"
+};
---
+++ theme.config.jsx
@@ -0,0 +1,7 @@
+export default {
+  logo: <h2>quarkle</h2>,
+  project: {
+    link: "https://github.com/shuding/nextra",
+  },
+  // ... other theme options
+};
